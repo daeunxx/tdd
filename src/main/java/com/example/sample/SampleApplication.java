bec0aea38a767fb82bc3419fe56ced87f12673f6
daeun <sss3598@gmail.com>
2024-09-23 22:45:42 +0900

    계산기 테스트 | 기본 연산 테스트

---
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -0,0 +1,21 @@
+package com.example.sample;
+
+import java.util.Scanner;
+
+public class SampleApplication {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter two numbers and an operator (e.g 1 + 2): ");
+
+    String result = scanner.nextLine();
+    String[] parts = result.split(" ");
+
+    long num1 = Long.parseLong(parts[0]);
+    long num2 = Long.parseLong(parts[2]);
+    String operator = parts[1];
+
+    Calculator calculator = new Calculator();
+    System.out.println(calculator.calculate(num1, num2, operator));
+  }
+}
