6912321f35b41a9ad152534d7069d8dea3af4249
daeun <sss3598@gmail.com>
2024-09-23 23:30:29 +0900

    계산기 테스트 | 요청 객체 validation

--- src/main/java/com/example/sample/CalculationRequest.java
+++ src/main/java/com/example/sample/CalculationRequest.java
@@ -7,9 +7,26 @@ public class CalculationRequest {
   private final String operator;
 
   public CalculationRequest(String[] parts) {
+
+    if (parts.length != 3) {
+      throw new BadRequestException();
+    }
+
+    if (parts[1].length() != 1 ||
+        isInvalidOperator(parts[1])) {
+      throw new InvalidOperatorException();
+    }
+
     this.num1 = Long.parseLong(parts[0]);
     this.num2 = Long.parseLong(parts[2]);
     this.operator = parts[1];
+  }
+
+  private boolean isInvalidOperator(String operator) {
+    return !operator.equals("+") &&
+        !operator.equals("-") &&
+        !operator.equals("*") &&
+        !operator.equals("/");
   }
 
   public long getNum1() {
