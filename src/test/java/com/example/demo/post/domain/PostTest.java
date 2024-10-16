1aa7e8a04e7a82f85fcafca8615fc154036011c7
daeun <sss3598@gmail.com>
2024-10-16 22:25:09 +0900

    리팩토링 | PostService, PostController 도메인과 영속성 객체 구분

---
+++ src/test/java/com/example/demo/post/domain/PostTest.java
@@ -0,0 +1,20 @@
+package com.example.demo.post.domain;
+
+import org.junit.jupiter.api.Test;
+
+class PostTest {
+
+  @Test
+  public void PostCreate_객체로_Post_생성() {
+    //given
+    //when
+    //then
+  }
+
+  @Test
+  public void PostUpdate_객체로_Post_수정() {
+    //given
+    //when
+    //then
+  }
+}
