625a499a384b55a55e3a5f5c2e46a8ddc06ff037
daeun <sss3598@gmail.com>
2024-10-16 22:06:06 +0900

    리팩토링 | UserService, UserController 도메인과 영속성 객체 구분

--- src/main/java/com/example/demo/user/domain/User.java
+++ src/main/java/com/example/demo/user/domain/User.java
@@ -1,18 +1,21 @@
 package com.example.demo.user.domain;
 
+import com.example.demo.common.domain.exception.CertificationCodeNotMatchedException;
+import java.time.Clock;
+import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class User {
 
-  private Long id;
-  private String email;
-  private String nickname;
-  private String address;
-  private String certificationCode;
-  private UserStatus status;
-  private Long lastLoginAt;
+  private final Long id;
+  private final String email;
+  private final String nickname;
+  private final String address;
+  private final String certificationCode;
+  private final UserStatus status;
+  private final Long lastLoginAt;
 
   @Builder
   public User(Long id, String email, String nickname, String address, String certificationCode,
@@ -24,5 +27,55 @@ public class User {
     this.certificationCode = certificationCode;
     this.status = status;
     this.lastLoginAt = lastLoginAt;
+  }
+
+  public static User create(UserCreate userCreate) {
+    return User.builder()
+        .email(userCreate.getEmail())
+        .nickname(userCreate.getNickname())
+        .address(userCreate.getAddress())
+        .status(UserStatus.PENDING)
+        .certificationCode(UUID.randomUUID().toString())
+        .build();
+  }
+
+  public User update(UserUpdate userUpdate) {
+    return User.builder()
+        .id(this.id)
+        .email(this.email)
+        .nickname(userUpdate.getNickname())
+        .address(userUpdate.getAddress())
+        .certificationCode(this.certificationCode)
+        .status(this.status)
+        .lastLoginAt(this.lastLoginAt)
+        .build();
+  }
+
+  public User login() {
+    return User.builder()
+        .id(this.id)
+        .email(this.email)
+        .nickname(this.getNickname())
+        .address(this.getAddress())
+        .certificationCode(this.certificationCode)
+        .status(this.status)
+        .lastLoginAt(Clock.systemUTC().millis())
+        .build();
+  }
+
+  public User certificate(String certificationCode) {
+    if (!certificationCode.equals(this.certificationCode)) {
+      throw new CertificationCodeNotMatchedException();
+    }
+
+    return User.builder()
+        .id(this.id)
+        .email(this.email)
+        .nickname(this.getNickname())
+        .address(this.getAddress())
+        .certificationCode(this.certificationCode)
+        .status(UserStatus.ACTIVE)
+        .lastLoginAt(this.lastLoginAt)
+        .build();
   }
 }
---
+++ src/test/java/com/example/demo/user/domain/UserTest.java
@@ -0,0 +1,41 @@
+package com.example.demo.user.domain;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+  @Test
+  public void UserCreate_객체로_User_생성() {
+    //given
+    //when
+    //then
+  }
+
+  @Test
+  public void UserUpdate_객체로_User_수정() {
+    //given
+    //when
+    //then
+  }
+
+  @Test
+  public void User_로그인_후_마지막_로그인_시간_변경() {
+    //given
+    //when
+    //then
+  }
+
+  @Test
+  public void 유효한_인증_코드로_계정_활성화() {
+    //given
+    //when
+    //then
+  }
+
+  @Test
+  public void 유효하지_않은_인증_코드는_에러() {
+    //given
+    //when
+    //then
+  }
+}
