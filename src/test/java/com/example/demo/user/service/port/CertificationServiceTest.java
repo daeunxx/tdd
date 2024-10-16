9a64805d4b4b06c14762c3bd135e0f2a343fd8c6
daeun <sss3598@gmail.com>
2024-10-16 16:44:47 +0900

    리팩토링 | MailSender 테스트

---
+++ src/test/java/com/example/demo/user/service/port/CertificationServiceTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.user.service.port;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.demo.mock.FakeMailSender;
+import org.junit.jupiter.api.Test;
+
+class CertificationServiceTest {
+
+  @Test
+  void 이메일_전송() {
+    // given
+    FakeMailSender fakeMailSender = new FakeMailSender();
+    CertificationService certificationService = new CertificationService(fakeMailSender);
+
+    // when
+    certificationService.sendCertificationEmail(1, "sss3598@gmail.com",
+        "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+
+    // then
+    assertThat(fakeMailSender.email).isEqualTo("sss3598@gmail.com");
+    assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+    assertThat(fakeMailSender.content).isEqualTo(
+        "Please click the following link to certify your email address: http://localhost:8080/api/users/1/verify?certificationCode=aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+  }
+}
