998d793a466f7f13d49bd644d55ee96f23804d64
daeun <sss3598@gmail.com>
2024-10-19 23:58:19 +0900

    테스트 | PostController, PostCreateController

--- src/test/java/com/example/demo/mock/TestContainer.java
+++ src/test/java/com/example/demo/mock/TestContainer.java
@@ -1,5 +1,71 @@
 package com.example.demo.mock;
 
+import com.example.demo.common.service.port.ClockHolder;
+import com.example.demo.common.service.port.UuidHolder;
+import com.example.demo.post.controller.PostController;
+import com.example.demo.post.controller.PostCreateController;
+import com.example.demo.post.controller.port.PostService;
+import com.example.demo.post.service.PostServiceImpl;
+import com.example.demo.post.service.port.PostRepository;
+import com.example.demo.user.controller.MyInfoController;
+import com.example.demo.user.controller.UserController;
+import com.example.demo.user.controller.UserCreateController;
+import com.example.demo.user.controller.port.UserService;
+import com.example.demo.user.service.CertificationService;
+import com.example.demo.user.service.UserServiceImpl;
+import com.example.demo.user.service.port.MailSender;
+import com.example.demo.user.service.port.UserRepository;
+import lombok.Builder;
+
 public class TestContainer {
 
+  public final MailSender mailSender;
+  public final PostRepository postRepository;
+  public final UserRepository userRepository;
+
+  public final CertificationService certificationService;
+  public final PostService postService;
+  public final UserService userService;
+
+  public final UserController userController;
+  public final UserCreateController userCreateController;
+  public final MyInfoController myInfoController;
+  public final PostController postController;
+  public final PostCreateController postCreateController;
+
+  @Builder
+  public TestContainer(UuidHolder uuidHolder, ClockHolder clockHolder) {
+    this.mailSender = new FakeMailSender();
+    this.postRepository = new FakePostRepository();
+    this.userRepository = new FakeUserRepository();
+
+    this.certificationService = new CertificationService(mailSender);
+    this.postService = PostServiceImpl.builder()
+        .postRepository(postRepository)
+        .userRepository(userRepository)
+        .clockHolder(clockHolder)
+        .build();
+    this.userService = UserServiceImpl.builder()
+        .userRepository(userRepository)
+        .certificationService(certificationService)
+        .uuidHolder(uuidHolder)
+        .clockHolder(clockHolder)
+        .build();
+
+    this.userController = UserController.builder()
+        .userService(userService)
+        .build();
+    this.userCreateController = UserCreateController.builder()
+        .userService(userService)
+        .build();
+    this.myInfoController = MyInfoController.builder()
+        .userService(userService)
+        .build();
+    this.postController = PostController.builder()
+        .postService(postService)
+        .build();
+    this.postCreateController = PostCreateController.builder()
+        .postService(postService)
+        .build();
+  }
 }
---
+++ src/test/java/com/example/demo/post/controller/PostControllerTest.java
@@ -0,0 +1,106 @@
+package com.example.demo.post.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.example.demo.common.domain.exception.ResourceNotFoundException;
+import com.example.demo.mock.TestClockHolder;
+import com.example.demo.mock.TestContainer;
+import com.example.demo.post.controller.response.PostResponse;
+import com.example.demo.post.domain.Post;
+import com.example.demo.post.domain.PostUpdate;
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+class PostControllerTest {
+
+  @Test
+  void 게시글_조회() {
+    //given
+    TestContainer testContainer = TestContainer.builder().build();
+    User user = User.builder()
+        .id(1L)
+        .email("sss3598@gmail.com")
+        .nickname("daeun")
+        .address("Seoul")
+        .status(UserStatus.ACTIVE)
+        .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa")
+        .build();
+
+    Post post = Post.builder()
+        .id(1L)
+        .content("helloWorld")
+        .createdAt(100L)
+        .writer(user)
+        .build();
+
+    testContainer.userRepository.save(user);
+    testContainer.postRepository.save(post);
+
+    //when
+    ResponseEntity<PostResponse> result = testContainer.postController.getPostById(post.getId());
+
+    //then
+    assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(result.getBody()).isNotNull();
+    assertThat(result.getBody().getContent()).isEqualTo("helloWorld");
+    assertThat(result.getBody().getWriter().getNickname()).isEqualTo("daeun");
+    assertThat(result.getBody().getCreatedAt()).isEqualTo(100L);
+  }
+
+  @Test
+  void 존재하지_않은_게시글_조회() {
+    //given
+    TestContainer testContainer = TestContainer.builder().build();
+
+    //when
+    //then
+    assertThatThrownBy(() -> testContainer.postController.getPostById(1L)).isInstanceOf(
+        ResourceNotFoundException.class);
+
+  }
+
+  @Test
+  void 게시글_수정() {
+    //given
+    TestContainer testContainer = TestContainer.builder()
+        .clockHolder(new TestClockHolder(200L))
+        .build();
+    User user = User.builder()
+        .id(1L)
+        .email("sss3598@gmail.com")
+        .nickname("daeun")
+        .address("Seoul")
+        .status(UserStatus.ACTIVE)
+        .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa")
+        .build();
+
+    Post post = Post.builder()
+        .id(1L)
+        .content("helloWorld")
+        .createdAt(100L)
+        .writer(user)
+        .build();
+
+    testContainer.userRepository.save(user);
+    testContainer.postRepository.save(post);
+
+    //when
+    PostUpdate postUpdate = PostUpdate.builder()
+        .content("hiWorld")
+        .build();
+
+    ResponseEntity<PostResponse> result = testContainer.postController.updatePost(
+        post.getId(), postUpdate);
+
+    //then
+    assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(result.getBody()).isNotNull();
+    assertThat(result.getBody().getContent()).isEqualTo("hiWorld");
+    assertThat(result.getBody().getModifiedAt()).isEqualTo(200L);
+  }
+}
